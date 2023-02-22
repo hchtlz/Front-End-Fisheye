bca99dd40253e39c08fc2ce63f5bd9418b7d6115
Hugo Chtlz <93914147+hchtlz@users.noreply.github.com>
2023-02-22 14:07:57 +0100

    UPD : position of close cross of lightbox

--- scripts/models/gallery.js
+++ scripts/models/gallery.js
@@ -1,14 +1,9 @@
 export class Gallery {
     constructor (data) {
         this._medias = data
-        this._title = data.title
     }
 
     get medias() {
         return this._medias
-    }
-
-    get title() {
-        return this._title
     }
 }
